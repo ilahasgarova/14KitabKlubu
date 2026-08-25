46ad5a553cc9a2d1dbe4c938b02544905d244714
ilahasgarova <ilahasgerova@gmail.com>
2026-08-25 21:23:45 +0400

    Add Dockerfile to root

--- obj/Debug/net10.0/KitabKlubu.AssemblyInfo.cs
+++ obj/Debug/net10.0/KitabKlubu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e1d0d4ca4f06a80b9763ef7f0d75fa332e007d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54544ad2cf7afbee82aa5a783c6dbfce12c866f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
