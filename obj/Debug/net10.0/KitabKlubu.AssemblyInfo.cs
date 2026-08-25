54544ad2cf7afbee82aa5a783c6dbfce12c866f1
ilahasgarova <ilahasgerova@gmail.com>
2026-08-25 21:02:08 +0400

    Add Dockerfile

--- obj/Debug/net10.0/KitabKlubu.AssemblyInfo.cs
+++ obj/Debug/net10.0/KitabKlubu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4b25d57953a4c98d30ca8eb95e1299175474ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e1d0d4ca4f06a80b9763ef7f0d75fa332e007d")]
 [assembly: System.Reflection.AssemblyProductAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KitabKlubu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
